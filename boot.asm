6b6b1f92d2f8a1bfcfea265a2078f2b96d387154
daniil <daniil_evsienko@mail.ru>
2020-04-11 16:01:03 +0400

    Added write_str function

--- boot.asm
+++ boot.asm
@@ -1,10 +1,32 @@
 org		0x7c00					; We are loaded by BIOS at 0x7C00
- 
-Start:
+
+jmp start
+
+boot_msg db "My OS boot loader. Version 0.01",13,10,0
+
+; Вывод строки DS:SI на экран
+write_str:
+	push ax si
+	mov ah, 0x0E
+ @@:
+	lodsb
+	test al, al
+	jz @f
+	int 0x10
+	jmp @b
+ @@:
+	pop si ax
+	ret
+
+start:
  
 	cli							; Clear all Interrupts
+
+	mov si, boot_msg
+	call write_str
+
 	hlt							; halt the system
 	
 times 510 - ($-$$) db 0			; We have to be 512 bytes. Clear the rest of the bytes with 0
  
-dw 0xAA54						; Boot Signiture
+dw 0xAA55						; Boot Signiture
