224670899b5d84ad76e57c08953441cfedf28c3e
daniil <daniil_evsienko@mail.ru>
2020-06-10 10:56:57 +0400

    Added simple C kernel (fixed)

--- src/kernel.c
+++ src/kernel.c
@@ -1,12 +1,10 @@
 void kernel_main() {
 	char *screen_buffer = (void*)0xB8000;
-	char msg[] = "Hello, World!";
+	char *msg = "Hello world!";
 	unsigned int i = 24 * 80;
-	screen_buffer[i * 2] = *msg;
-	int j = 0;
-	while (msg[j] != '\0') {
-		screen_buffer[i * 2] = *(msg + j);
-		j++;
+	while (*msg) {
+		screen_buffer[i * 2] = *msg;
+		msg++;
 		i++;
-	}	
-}
+	}
+} 
